7ff9c88d36e3f85dcc80254ccb342d1f16b790bf
MuhammedAli21 <mohamedmohamed20211@gmail.com>
2024-10-28 00:54:48 +0300

    - adding Device Inspection. - adding scanner.

--- lib/Core/utils/strings.dart
+++ lib/Core/utils/strings.dart
@@ -36,4 +36,12 @@ class StringManager {
   static const String takePhoto = "Take a Photo";
   static const String chooseFromGallery = "Choose from Gallery";
   static const String close = "Close";
+  static const String deviceInspection = "Device Inspection";
+  static const String deviceInspectionID = "ID";
+  static const String deviceInspectionLastScanned = "Last Scanned";
+  static const String deviceInspectionDeviceCondition = "Device Condition";
+  static const String deviceInspectionBaitCondition = "Bait Condition";
+  static const String deviceInspectionRemoved = "Removed";
+  static const String deviceInspectionAddMaterial = "Add Material";
+  static const String deviceInspectionAddPest = "Add Pest";
 }
---
+++ lib/Features/device_inspection/presentation/widgets/custom_text.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomText extends StatelessWidget {
+  String title;
+  CustomText({required this.title});
+  @override
+  Widget build(BuildContext context) {
+    return Text(title,
+    style: Theme.of(context).textTheme.titleSmall!.copyWith(
+      fontSize: 25.sp,
+    ),);
+  }
+}

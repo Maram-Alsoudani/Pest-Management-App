e084dd9e4326074f7dadbdabdbdfcda17d378f28
El3osa <hhhmohamed91@gmai.com>
2024-10-30 20:16:17 +0300

    added auto login and added shared_file.

--- lib/Core/utils/strings.dart
+++ lib/Core/utils/strings.dart
@@ -50,4 +50,6 @@ class StringManager {
   static const String ok = "Ok";
   static const String registerSuccessfully = "Register Successfully.";
   static const String failed = "Failed";
+  static const String keyUserAdmin = "user_admin";
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,43 +3,60 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pesticides/Config/routes/routes_manger.dart';
+import 'package:pesticides/Core/utils/strings.dart';
 import 'package:pesticides/di/di.dart';
 
 import 'Config/theme/theming.dart';
+import 'Core/utils/SharedPrefsLocal.dart';
 import 'Features/register/presentation/manager/register_view_model_cubit.dart';
 import 'firebase_options.dart';
 
-void main()async{
-
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await SharedPrefsLocal.init();
+  var route = autoLogin();
   configureDependencies();
   runApp(MultiBlocProvider(
       providers: [
-        BlocProvider(create: (context) => getIt<RegisterViewModelCubit>(),)
+        BlocProvider(
+          create: (context) => getIt<RegisterViewModelCubit>(),
+        )
       ],
-      child: MyApp()));
+      child: MyApp(
+        route: route,
+      )));
 }
 
+String autoLogin() {
+  var item = SharedPrefsLocal.getData(key: StringManager.keyUserAdmin);
+  String route;
+  if (item != null) {
+    route = RoutesManger.routeNameCategoryScreen;
+  } else {
+    route = RoutesManger.routeNameEngOwnerScreen;
+  }
+  return route;
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final String route;
+  const MyApp({super.key, required this.route});
 
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize:  const Size(412, 892),
+      designSize: const Size(412, 892),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (_ , child) {
-        return  MaterialApp(
+      builder: (_, child) {
+        return MaterialApp(
           debugShowCheckedModeBanner: false,
-          initialRoute: RoutesManger.routeNameEngOwnerScreen  ,
+          initialRoute: route,
           routes: RoutesManger.route,
           theme: MyTheme.theme,
-
         );
       },
     );
